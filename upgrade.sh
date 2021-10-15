8e767526129ea52b3b8b829cb89e81906ada0548
AntiCompositeNumber <anticompositenumber@gmail.com>
2021-10-15 13:05:00 -0400

    fixup! migrate to Poetry

--- upgrade.sh
+++ upgrade.sh
@@ -2,11 +2,27 @@
 # coding: utf-8
 # SPDX-License-Identifier: CC0-1.0
 
-# Automatically install/upgrade Python dependencies in a virtual environment
-# Assumes the venv is in `venv` in the current directory.
-# Pip, and wheel will be upgraded to the latest version. All other
-# dependencies are installed based on poetry.lock. This script should be called
-# from the root directory of the repository.
+################################################################################
+# Automatically install/upgrade Python dependencies in a virtual environment.  #
+#                                                                              #
+# The virtual environment is assumed to be in the same directory as upgrade.sh #
+# and called `venv`, unless another virtual environment has already been       #
+# activated. Pip and wheel will be upgraded to the latest version. All other   #
+# dependencies are installed based on poetry.lock.                             #
+################################################################################
 
-venv/bin/pip install --upgrade pip wheel && pip install .
+# exit on error
+set -e
 
+PROJECT_PATH = $(dirname "$0")
+
+if [ -z "$VIRTUAL_ENV" ]
+then
+    # already in a venv, just use that
+    PIP_PATH = "pip"
+else
+    PIP_PATH = "$PROJECT_PATH/venv/bin/pip"
+fi
+
+$PIP_PATH install --upgrade pip wheel
+$PIP_PATH install $PROJECT_PATH
