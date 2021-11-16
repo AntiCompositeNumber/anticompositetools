87c34b9d016a801d90ff699edc1fbab7986fcc09
AntiCompositeNumber <anticompositenumber@gmail.com>
2021-11-15 19:50:34 -0500

    [WIP] commit to switch branch

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 
-# Copyright 2019 AntiCompositeNumber
+# Copyright 2021 AntiCompositeNumber
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,10 +17,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-import sys
+"""Toolforge uwsgi stub"""
 
-sys.path.append(os.getcwd()[:-4])
-from src import create_app  # noqa E402
+from anticompositetools import create_app
 
 app = create_app()
