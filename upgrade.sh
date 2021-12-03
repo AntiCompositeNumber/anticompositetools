678cc8a43446a62b06b089a231081894ca175848
AntiCompositeNumber <anticompositenumber@gmail.com>
2021-12-03 14:18:10 -0500

    Include correct upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -4,9 +4,14 @@
 
 # Automatically install/upgrade Python dependencies in a virtual environment
 # Assumes the venv is in `venv` in the current directory.
-# Pip, and wheel will be upgraded to the latest version. All other
+# Pip, wheel, and micropipenv will be upgraded to the latest version. All other
 # dependencies are installed based on poetry.lock. This script should be called
 # from the root directory of the repository.
 
-venv/bin/pip install --upgrade pip wheel && pip install .
+source venv/bin/activate
 
+pip install --upgrade pip wheel 'micropipenv[toml]'
+
+MICROPIPENV_NO_LOCKFILE_PRINT=1 MICROPIPENV_NO_LOCKFILE_WRITE=1 micropipenv install
+
+deactivate
